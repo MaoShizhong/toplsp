b5e6a64f18d2e871225b5313d5feac8874f15e02
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-03-29 00:58:16 +0100

    Add passing test

--- encoder.test.js
+++ encoder.test.js
@@ -2,9 +2,10 @@ import encodeMessage from "./encoder";
 
 describe("Encode properly", () => {
   test("Encode correct length", () => {
-    const msg = { hello: "World", thiskey: 22 };
-    const json = JSON.stringify(msg);
+    const msg = '{hello:"World",thiskey:22}';
     const result = encodeMessage(msg);
-    expect(result).toBe(`Content-Length: ${json.length}\r\n\r\n${json}`);
+    expect(result).toBe(
+      `Content-Length: 30\r\n\r\n"{hello:\\"World\\",thiskey:22}"`,
+    );
   });
 });
