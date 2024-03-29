7f80d75567c56d5c06bc2145df4c3995a9089bb2
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-03-29 09:42:13 +0100

    Add more tests

--- encoder.test.js
+++ encoder.test.js
@@ -2,10 +2,22 @@ import encodeMessage from "./encoder";
 
 describe("Encode properly", () => {
   test("Encode correct length", () => {
-    const msg = '{hello:"World",thiskey:22}';
-    const result = encodeMessage(msg);
+    const ob = { hello: "World", thiskey: 22 };
+    const result = encodeMessage(ob);
     expect(result).toBe(
-      `Content-Length: 30\r\n\r\n"{hello:\\"World\\",thiskey:22}"`,
+      'Content-Length: 30\r\n\r\n{"hello":"World","thiskey":22}',
+    );
+  });
+
+  test("Encode nested objects", () => {
+    const ob = {
+      nested: [{ world: "Hello" }],
+      thiskey: 23,
+      another: { nested: { object: true } },
+    };
+    const result = encodeMessage(ob);
+    expect(result).toBe(
+      'Content-Length: 80\r\n\r\n{"nested":[{"world":"Hello"}],"thiskey":23,"another":{"nested":{"object":true}}}',
     );
   });
 });
