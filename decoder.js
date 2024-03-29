c832f8b38254a48e23212e0c01f545fe901fa5b6
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-03-29 10:13:19 +0100

    Fix test issues

--- decoder.js
+++ decoder.js
@@ -1,3 +1,10 @@
 export default function decodeMessage(msg) {
-  return {};
+  const startIndex = "Content-Length ".length;
+
+  const lastIndex = msg.indexOf("\r\n\r\n");
+  const length = Number(msg.substring(startIndex, lastIndex));
+
+  const obStart = lastIndex + "\r\n\r\n".length;
+  const obj = msg.substring(obStart, obStart + length);
+  return JSON.parse(obj);
 }
--- decoder.test.js
+++ decoder.test.js
@@ -2,8 +2,14 @@ import decodeMessage from "./decoder";
 
 describe("Decode properly", () => {
   test("Decode empty object", () => {
-    let str = "Content-Length 2\r\r\n\n{}";
+    let str = "Content-Length 2\r\n\r\n{}";
     const result = decodeMessage(str);
     expect(result).toEqual({});
+  });
+
+  test("Decode object correctly", () => {
+    let str = 'Content-Length 14\r\n\r\n{"name":"Joe"}';
+    const result = decodeMessage(str);
+    expect(result).toEqual({ name: "Joe" });
   });
 });
