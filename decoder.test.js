88b0cbc39dc80371342f7031223089b823d176da
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-03-29 10:22:12 +0100

    Add testing complex object

--- decoder.test.js
+++ decoder.test.js
@@ -2,14 +2,25 @@ import decodeMessage from "./decoder";
 
 describe("Decode properly", () => {
   test("Decode empty object", () => {
-    let str = "Content-Length 2\r\n\r\n{}";
+    const str = "Content-Length 2\r\n\r\n{}";
     const result = decodeMessage(str);
     expect(result).toEqual({});
   });
 
   test("Decode object correctly", () => {
-    let str = 'Content-Length 14\r\n\r\n{"name":"Joe"}';
+    const str = 'Content-Length 14\r\n\r\n{"name":"Joe"}';
     const result = decodeMessage(str);
     expect(result).toEqual({ name: "Joe" });
+  });
+
+  test("Decode complext nested object", () => {
+    const str =
+      'Content-Length: 78\r\n\r\n{"nested":[{"man":"woman"}],"thiskey":23,"another":{"nested":{"object":true}}}';
+    const result = decodeMessage(str);
+    expect(result).toEqual({
+      nested: [{ man: "woman" }],
+      thiskey: 23,
+      another: { nested: { object: true } },
+    });
   });
 });
