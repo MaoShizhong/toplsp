5a2ef1c50e3dfe6c8800149e1f920aa6ad8d576f
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-03-29 12:01:44 +0100

    Add logging

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
+import fs from "fs";
 import decodeMessage from "./decoder.js";
 
 process.stdin.on("data", (data) => {
   try {
     const obj = decodeMessage(data.toString());
+    fs.appendFile("./logger.txt", `${Date.now()}: ${data}\n`, () => {});
     handleMessage(obj);
-  } catch {
-    console.log(data.toString());
+  } catch (e) {
+    fs.appendFile("./error.txt", `${Date.now()}: ${e}\n`, () => {});
   }
 });
 
-function handleMessage(obj) {
-  console.log("Handling");
-}
+function handleMessage(obj) {}
