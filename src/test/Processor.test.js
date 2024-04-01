c9c3b23ee0c6e7a358d9a32d17b5937ff0b5b2f4
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-04-01 13:55:21 +0200

    Update test formatting for printing

--- src/test/Processor.test.js
+++ src/test/Processor.test.js
@@ -4,12 +4,12 @@ describe("Prints state correctly", () => {
   test("Prints one state", () => {
     const proc = new Processor();
     proc.updateState("home", "down the street");
-    expect(proc.toString()).toBe("home:\n[\ndown the street\n]");
+    expect(proc.toString()).toBe('home: "down the street"');
   });
 
   test("Print arrays correctly", () => {
     const proc = new Processor();
-    proc.updateState("home", [{ text: "down the street" }]);
-    expect(proc.toString()).toBe("home:\n[\ndown the street\n]");
+    proc.updateState("work", [{ text: "away from\nthe street" }]);
+    expect(proc.toString()).toBe('home: "away from\the street"');
   });
 });
