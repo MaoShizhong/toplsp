20a8357a27c446fd25c0a2ee19d18680696f4e62
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-04-01 13:33:10 +0200

    Update logger formatting

--- src/logger.js
+++ src/logger.js
@@ -2,5 +2,5 @@ import fs from "fs";
 
 export default function logger(title, msg) {
   const time = new Date().toISOString();
-  fs.appendFile("./logs.txt", `${time}: ${title}\n\t\t${msg}\n`, () => {});
+  fs.appendFile("./logs.txt", `${time}: ${title}\n  ${msg}\n`, () => {});
 }
