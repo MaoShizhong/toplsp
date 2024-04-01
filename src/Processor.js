fd57665e6ffce0d25011691ae8dd94bcd9acd147
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-04-01 13:13:56 +0200

    Add processor for state

---
+++ src/Processor.js
@@ -0,0 +1,13 @@
+class Proccessor {
+  constructor() {
+    this.state = new Map();
+  }
+
+  updateState(key, value) {
+    this.state.set(key, value);
+  }
+
+  getState(key) {
+    return this.state.get(key);
+  }
+}
