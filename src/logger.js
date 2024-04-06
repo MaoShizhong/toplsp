1698023d1fef89e92d503cc62d899449df309ff9
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-04-06 14:03:31 +0200

    Update logger

--- src/logger.js
+++ src/logger.js
@@ -3,7 +3,7 @@ import fs from "fs";
 export default function (title, msg) {
   const time = new Date().toISOString();
   fs.appendFile(
-    "./logs.txt",
+    "~/toplsp-logs.txt",
     `${time}: ${title}\n  ${msg}\n========================================\n`,
     () => {},
   );
