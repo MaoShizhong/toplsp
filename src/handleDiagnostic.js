2f3fe925c03b5daf913d84b69eb299c9fbb3edf4
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-04-08 12:30:24 +0200

    Add pushing diagnostics

--- src/handleDiagnostic.js
+++ src/handleDiagnostic.js
@@ -1,5 +1,24 @@
 import logger from "./logger.js";
+import { encodeMessage } from "./parser.js";
 
 export default function (state, msg) {
+  const uri = msg.params.textDocument.uri;
+  const diagnostics = parseDiagnostics(state.get(uri));
+
+  const response = encodeMessage({ id: msg.id, uri: msg, diagnostics });
+  console.log(response);
   logger(msg.method, "Handling diagnostics");
+}
+
+function parseDiagnostics(text) {
+  return [
+    {
+      range: {
+        start: { line: 0, character: 0 },
+        end: { line: 0, character: 5 },
+      },
+      severity: 1,
+      message: "This is diagnostic test",
+    },
+  ];
 }
