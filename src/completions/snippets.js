074787fd7361e39973e3177bedf69a9f0645787e
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-04-08 11:26:46 +0200

    Fix indentation for snippet

--- src/completions/snippets.js
+++ src/completions/snippets.js
@@ -6,8 +6,8 @@ export default [
     kind,
     label: "Javascript",
     detail: "Javascript snippet",
-    sortText: "javascript",
-    filterText: "sss```javascript",
+    sortText: "```javascript",
+    filterText: "```javascript",
     insertTextFormat,
     insertText:
       '```javascript\n\t${1:const obj = {\n\t\tname: "object"\n\t\tmarker: "X"\n\t}}\n```\n$0',
