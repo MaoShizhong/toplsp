fe6d09d2eef091630029acec75b92db46bb25beb
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-04-08 14:06:31 +0200

    Add first introduction diagnostic

--- src/handleDiagnostic.js
+++ src/handleDiagnostic.js
@@ -14,14 +14,19 @@ export default function (state, msg) {
 }
 
 function parseDiagnostics(text) {
-  return [
-    {
+  const diagnostics = [];
+  const lines = text.split("\n");
+  if (lines[0] !== "### Introduction") {
+    diagnostics.push({
       range: {
         start: { line: 0, character: 0 },
-        end: { line: 0, character: 5 },
+        end: { line: 0, character: lines[0].length },
       },
-      severity: 1,
-      message: "This is diagnostic test",
-    },
-  ];
+      sevirity: 2,
+      message:
+        "There should be an introduction section on the top of the document",
+    });
+  }
+
+  return diagnostics;
 }
