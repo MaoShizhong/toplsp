5c551a20464cdfe0a7120ac4b3f55dd26c42e5f5
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-04-08 11:40:42 +0200

    Update logger logging location

--- src/logger.js
+++ src/logger.js
@@ -3,7 +3,7 @@ import fs from "fs";
 export default function (title, msg) {
   const time = new Date().toISOString();
   fs.appendFile(
-    "~/toplsp-logs.txt",
+    "./logs.txt",
     `${time}: ${title}\n  ${msg}\n========================================\n`,
     () => {},
   );
