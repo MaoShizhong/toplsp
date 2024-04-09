3f38178bd2666e2b9cf481d01e7539e7787b1cd3
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-04-09 11:08:05 +0200

    Add bash snippet

--- src/completions/snippets.js
+++ src/completions/snippets.js
@@ -12,4 +12,13 @@ export default [
     insertText:
       '```javascript\n\t${1:const obj = {\n\t\tname: "object"\n\t\tmarker: "X"\n\t}}\n```\n$0',
   },
+  {
+    kind,
+    label: "Bash",
+    detail: "Bash snippet",
+    sortText: "```bash",
+    filterText: "```bash",
+    insertTextFormat,
+    insertText: "```bash\n${1:cat file.txt}\n```\n$0",
+  },
 ];
