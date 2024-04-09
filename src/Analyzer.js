56d7de779a76df65b3b16bb734a2efe3d2787b21
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-04-09 12:28:57 +0200

    Move diagnostics into analyzer

--- src/Analyzer.js
+++ src/Analyzer.js
@@ -8,4 +8,22 @@ export default class Analyzer {
   getContent(uri) {
     return this.#state.get(uri) ?? "";
   }
+
+  generateDiagnostics(uri) {
+    const diagnostics = [];
+    const lines = this.getContent(uri).split("\n");
+    if (lines[0] !== "### Introduction") {
+      diagnostics.push({
+        range: {
+          start: { line: 0, character: 0 },
+          end: { line: 0, character: lines[0].length },
+        },
+        sevirity: 2,
+        message:
+          "There should be an introduction section on the top of the document",
+      });
+    }
+
+    return diagnostics;
+  }
 }
