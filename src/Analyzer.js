9e96843fcd7f6966405e48a31544e1f7f2f6efc9
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-05-03 16:51:16 +0200

    Add top specific markdown

--- src/Analyzer.js
+++ src/Analyzer.js
@@ -1,7 +1,11 @@
 import Diagnostics from "./diagnostics/Diagnostics.js";
+import Diagnostic from "./diagnostics/Diagnostic.js";
+import top1 from "./diagnostics/top1.js";
+import MarkdownIt from "markdown-it";
 
 export default class Analyzer {
   #state = new Map();
+  #parser = new MarkdownIt();
 
   updateState(uri, content) {
     this.#state.set(uri, content);
@@ -13,25 +17,39 @@ export default class Analyzer {
 
   generateDiagnostics(uri) {
     const diagnostics = [];
-    const lines = this.getContent(uri).split("\n");
-    const sections = {
-      index: 0,
-      line: 0,
-      headers: ["### Introduction", "### Overview"],
+    const content = this.getContent(uri);
+    const params = {
+      parsers: { markdownit: { tokens: this.#parser.parse(content) } },
     };
-
-    lines.forEach((line, lineNumber) => {
-      if (line === sections.headers[sections.index]) {
-        sections.index = sections.index + 1;
-        sections.line = lineNumber + 1;
-      }
-    });
-
-    if (sections.index <= sections.headers.length) {
+    const onError = (errorObj) => {
       diagnostics.push(
-        this.getMissingSection(sections.headers[sections.index], sections.line),
+        new Diagnostic()
+          .line(errorObj.lineNumber ?? 0)
+          .character(0, 0)
+          .sevirityLevel(2)
+          .diagnosticMessage(errorObj.detail),
       );
-    }
+    };
+    top1(params, onError);
+    // diagnostics.push(token);
+    // const sections = {
+    //   index: 0,
+    //   line: 0,
+    //   headers: ["### Introduction", "### Overview"],
+    // };
+    //
+    // lines.forEach((line, lineNumber) => {
+    //   if (line === sections.headers[sections.index]) {
+    //     sections.index = sections.index + 1;
+    //     sections.line = lineNumber + 1;
+    //   }
+    // });
+    //
+    // if (sections.index <= sections.headers.length) {
+    //   diagnostics.push(
+    //     this.getMissingSection(sections.headers[sections.index], sections.line),
+    //   );
+    // }
 
     return diagnostics;
   }
