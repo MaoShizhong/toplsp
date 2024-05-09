29d0b97268f4fac22a73b50642b440aa809b2d61
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-05-09 13:14:42 +0200

    Add client

---
+++ client/src/extension.js
@@ -0,0 +1,50 @@
+import * as path from "path";
+import { workspace } from "vscode";
+import { LanguageClient, TransportKind } from "vscode-languageclient/node";
+
+let client;
+
+export function activate(context) {
+  // The server is implemented in node
+  const serverModule = context.asAbsolutePath(
+    path.join("server", "out", "server.js"),
+  );
+
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  const serverOptions = {
+    run: { module: serverModule, transport: TransportKind.stdio },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.stdio,
+    },
+  };
+
+  // Options to control the language client
+  const clientOptions = {
+    // Register the server for all documents by default
+    documentSelector: [{ scheme: "file", language: "markdown" }],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    "REPLACE_ME language-server-id",
+    "REPLACE_ME language server name",
+    serverOptions,
+    clientOptions,
+  );
+
+  // Start the client. This will also launch the server
+  client.start();
+}
+
+export function deactivate() {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
