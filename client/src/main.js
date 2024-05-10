4b8da55f2f286e72d428f8adf65e38c171bd8b33
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-05-10 09:27:34 +0200

    Add debugging option

--- client/src/main.js
+++ client/src/main.js
@@ -5,32 +5,27 @@ import { LanguageClient, TransportKind } from "vscode-languageclient/node";
 let client;
 
 export function activate(context) {
-  // The server is implemented in node
   const serverModule = context.asAbsolutePath(
     path.join("server", "out", "main.js"),
   );
 
-  // If the extension is launched in debug mode then the debug server options are used
-  // Otherwise the run options are used
+  const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
   const serverOptions = {
     run: { module: serverModule, transport: TransportKind.stdio },
     debug: {
       module: serverModule,
       transport: TransportKind.stdio,
+      options: debugOptions,
     },
   };
 
-  // Options to control the language client
   const clientOptions = {
-    // Register the server for all documents by default
     documentSelector: [{ scheme: "file", language: "markdown" }],
     synchronize: {
-      // Notify the server about file changes to '.clientrc files contained in the workspace
       fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
     },
   };
 
-  // Create the language client and start the client.
   client = new LanguageClient(
     "toplsp",
     "Top Language Server",
@@ -38,7 +33,6 @@ export function activate(context) {
     clientOptions,
   );
 
-  // Start the client. This will also launch the server
   client.start();
 }
 
