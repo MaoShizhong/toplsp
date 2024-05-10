9b4e5b58ebbc59baa00267b88d7ca3d8bef523b5
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-05-10 18:38:56 +0200

    Update code action to use various operations

--- server/src/CodeAction.js
+++ server/src/CodeAction.js
@@ -4,27 +4,39 @@ export default class CodeAction {
     this.kind = "quickfix";
     this.isPreferred = true;
     this.diagnostics = diagnostics;
+    const { fixInfo } = result;
 
-    const lineNumber = result.lineNumber - 1;
-    const characterStart = result.fixInfo.editColumn - 1;
-    const characterEnd = result.fixInfo.deleteCount + characterStart;
-    const { insertText } = result.fixInfo;
+    if (!fixInfo) {
+      throw new Error(
+        "Can't call constructor for results that doesn't contain fixInfo",
+      );
+    }
 
+    const lineStart = result.lineNumber - 1;
+
+    let lineEnd = fixInfo.lineNumber ? fixInfo.lineNumber - 1 : lineStart;
+    const characterStart = fixInfo.editColumn ? fixInfo.editColumn - 1 : 0;
+    let characterEnd = 0;
+    if (fixInfo.deleteCount === -1) {
+      lineEnd += 1;
+    } else if (fixInfo.deleteCount > 0) {
+      characterEnd = fixInfo.deleteCount + characterStart;
+    }
     this.edit = {
       changes: {
         [uri]: [
           {
             range: {
               start: {
-                line: lineNumber,
+                line: lineStart,
                 character: characterStart,
               },
               end: {
-                line: lineNumber,
+                line: lineEnd,
                 character: characterEnd,
               },
             },
-            newText: insertText,
+            newText: fixInfo.insertText ?? "",
           },
         ],
       },
