828bd9fa1183b9274e530d59906588e79ec204a3
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-05-16 13:49:47 +0200

    Fix configuration not being loaded correctly

--- server/src/Markdown.js
+++ server/src/Markdown.js
@@ -1,36 +1,9 @@
+import { parse } from "jsonc-parser";
+import fs from "fs";
+
 export default class Markdown {
   #projectConfig;
   #lessonConfig;
-
-  async initOptions(uri) {
-    // Wrap in try catch incase not working from TOP directory, exit gracefuly
-    let baseConfig;
-    let projectConfig;
-    let lessonConfig;
-    let configDirectory;
-    try {
-      configDirectory = this.#getConfigDirectory(uri);
-      baseConfig = this.#readBaseConfig(configDirectory);
-      projectConfig = this.#readProjectConfig(configDirectory);
-      lessonConfig = this.#readLessonConfig(configDirectory);
-    } catch (_) {
-      return;
-    }
-
-    const options = parse(baseConfig);
-    const rulePromises = options.customRules.map(
-      (r) => import(configDirectory + r.slice(2)),
-    );
-
-    const customRules = await Promise.all(rulePromises);
-    options.customRules = customRules.map((rule) => rule.default);
-
-    this.#projectConfig = parse(projectConfig);
-    this.#projectConfig.extends = options;
-
-    this.#lessonConfig = parse(lessonConfig);
-    this.#lessonConfig.extends = options;
-  }
 
   getOptions(uri) {
     if (uri.contains("project")) {
@@ -40,18 +13,49 @@ export default class Markdown {
     }
   }
 
+  async initOptions(uri) {
+    // Wrap in try catch incase not working from TOP directory, exit gracefuly
+    try {
+      const configDirectory = this.#getConfigDirectory(uri);
+      const baseConfig = this.#readBaseConfig(configDirectory);
+
+      const options = parse(baseConfig);
+      const rulePromises = options.customRules.map(
+        (r) => import(configDirectory + r.slice(2)),
+      );
+
+      const customRules = await Promise.all(rulePromises);
+      options.customRules = customRules.map((rule) => rule.default);
+
+      this.#initProjectConfig(options);
+      this.#initLessonConfig(options);
+    } catch (_) {
+      return;
+    }
+  }
+
+  #initProjectConfig(options, configDirectory) {
+    const path = `${configDirectory}project.markdownlint-cli2.jsonc`;
+    const configContent = fs.readFileSync(path).toString();
+    const config = parse(configContent);
+    this.#projectConfig = options;
+    Object.entries(config)
+      .filter((key) => key !== "extends")
+      .forEach((key, value) => (this.#projectConfig[key] = value));
+  }
+
+  #initLessonConfig(options, configDirectory) {
+    const path = `${configDirectory}lesson.markdownlint-cli2.jsonc`;
+    const configContent = fs.readFileSync(path).toString();
+    const config = parse(configContent);
+    this.#lessonConfig = options;
+    Object.entries(config)
+      .filter((key) => key !== "extends")
+      .forEach((key, value) => (this.#lessonConfig[key] = value));
+  }
+
   #readBaseConfig(configDirectory) {
     const path = `${configDirectory}.markdownlint-cli2.jsonc`;
-    return fs.readFileSync(path).toString();
-  }
-
-  #readProjectConfig(configDirectory) {
-    const path = `${configDirectory}project.markdownlint-cli2.jsonc`;
-    return fs.readFileSync(path).toString();
-  }
-
-  #readLessonConfig(configDirectory) {
-    const path = `${configDirectory}lesson.markdownlint-cli2.jsonc`;
     return fs.readFileSync(path).toString();
   }
 
