9695f693d22dfddc65f084fac1a937442b6d9ae6
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-05-16 11:57:26 +0200

    Extract configs into seperate variables

--- server/src/Markdown.js
+++ server/src/Markdown.js
@@ -1,30 +1,49 @@
 export default class Markdown {
+  #baseConfig;
   #projectConfig;
   #lessonConfig;
 
   async initOptions(uri) {
     // Wrap in try catch incase not working from TOP directory, exit gracefuly
-    let config;
-    let rootPath;
+    let baseConfig;
+    let projectConfig;
+    let lessonConfig;
+    let configDirectory;
     try {
-      rootPath = this.#getConfigurationPath(uri);
-      const configPath = rootPath + ".markdownlint-cli2.jsonc";
-      config = fs.readFileSync(configPath).toString();
+      configDirectory = this.#getConfigDirectory(uri);
+      baseConfig = this.#getBaseConfig(configDirectory);
+      projectConfig = this.#getProjectConfig(configDirectory);
+      lessonConfig = this.#getLessonConfig(configDirectory);
     } catch (_) {
       return;
     }
 
-    const options = parse(config);
+    const options = parse(baseConfig);
     const rulePromises = options.customRules.map(
-      (r) => import(rootPath + r.slice(2)),
+      (r) => import(configDirectory + r.slice(2)),
     );
 
     const customRules = await Promise.all(rulePromises);
     options.customRules = customRules.map((rule) => rule.default);
-    this.#options = options;
+    this.#baseConfig = options;
   }
 
-  #getConfigurationPath(uri) {
+  #getBaseConfig(configDirectory) {
+    const path = `${configDirectory}.markdownlint-cli2.jsonc`;
+    return fs.readFileSync(path).toString();
+  }
+
+  #getProjectConfig(configDirectory) {
+    const path = `${configDirectory}project.markdownlint-cli2.jsonc`;
+    return fs.readFileSync(path).toString();
+  }
+
+  #getLessonConfig(configDirectory) {
+    const path = `${configDirectory}lesson.markdownlint-cli2.jsonc`;
+    return fs.readFileSync(path).toString();
+  }
+
+  #getConfigDirectory(uri) {
     let startIndex = 0;
     if (uri.startsWith("file://")) {
       startIndex = "file://".length;
